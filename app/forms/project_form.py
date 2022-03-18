6cf66d1bac74210249652ce52a823cd21bd58210
Vu <vco.thien@gmail.com>
2022-03-18 08:42:18 -0700

    add works

--- app/forms/project_form.py
+++ app/forms/project_form.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError
 
 class ProjectForm(FlaskForm):
     title = StringField("Project", validators=[DataRequired()])
+    user_id = IntegerField("user_id")
+
+
