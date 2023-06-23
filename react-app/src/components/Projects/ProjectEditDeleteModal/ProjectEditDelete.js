083609cc83dd12c4b1458972ebfc8682a33f689f
Vu <vco.thien@gmail.com>
2023-06-23 16:43:49 -0700

    added padding

--- react-app/src/components/Projects/ProjectEditDeleteModal/ProjectEditDelete.js
+++ react-app/src/components/Projects/ProjectEditDeleteModal/ProjectEditDelete.js
@@ -1,17 +1,16 @@
 import DeleteFormModal from "../DeleteProject/DeleteModal";
-import EditProjectFormModal from "../EditProjectFormModal/EditModal"
+import EditProjectFormModal from "../EditProjectFormModal/EditModal";
 
 function ProjectEditDelete({ project }) {
-
-    return (
-        <div className="edit-buttons-container">
-            <div className="edit-delete-container">
-            <EditProjectFormModal project={project} />
-            <li class="menu_separator"></li>
-            <DeleteFormModal project={project} />
-            </div>
-        </div>
-    )
+  return (
+    <>
+      <div className="edit-buttons-container">
+          <EditProjectFormModal project={project} />
+          <li class="menu_separator"></li>
+          <DeleteFormModal project={project} />
+      </div>
+    </>
+  );
 }
 
 export default ProjectEditDelete;
